c641cbdb929deaf6eadecf8aa0bac1174207413b
Alex Alex <102162709+tastychef@users.noreply.github.com>
2022-07-27 19:30:57 +0300

    version_1

--- WIKI/цикл for.py
+++ WIKI/цикл for.py
@@ -1,4 +1,42 @@
-i = 1
-while i < 5:
-    print(i)
-    i += 1
+# классический пример цикла
+# for i in range(5):
+#     print(i)
+
+# цикл с указанием шага
+# for i in range(2, 10, 2):
+#     print(i)
+
+# перебор по порядку сверху вниз
+# a = "abcdefg"
+# for i in a:
+#     print(i)
+
+# перебор по порядку в 1-ну строку
+# a = "abcdefg"
+# for i in a:
+#     print(i, end="")
+# хз
+# # count = 0
+# # for i in range(5):
+# #     print("Hello World!")
+# #     count += 1
+# # print(count)
+# сумма последовательности
+# s = 0
+# for i in range(10, 100):
+#     s = s + i
+# print(s)
+# сумма последовательности
+# s = 0
+# for i in range(10, 100):
+#     s = s + i
+# print(s)
+# факториал (произведение последовательности)
+# n = int(input())
+# m = 1
+# for i in range(1, n+1):
+#     m = m * i
+# print(m)
+# n = int(input())
+# for i in range(n):
+#     print("aaaaaaa")
